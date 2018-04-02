082d145bc08e5639e6af22bd8914979238b4b632
msreicha <msreicha@users.noreply.github.com>
2018-04-02 14:11:58 +0200

    Played around with Eclipse Diff ... added second systemout for this.

--- PlayGroundJava/src/org/mreich/gittest/HelloGit.java
+++ PlayGroundJava/src/org/mreich/gittest/HelloGit.java
@@ -4,6 +4,7 @@ public class HelloGit {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Git world");
+		System.out.println("Let's see what Eclips can do wrt compare...");
 
 	}
 
